71500081f77642171a02c74fed3a1846bb65b627
Milan TURCANO <turcano.milan@sfr.fr>
2023-06-12 12:08:50 +0200

    ajout nouvelle div success subscrive a poursuive

--- main.js
+++ main.js
@@ -2,25 +2,25 @@ let input = document.querySelector(".inputEmail");
 let boutonValidation = document.querySelector(".button");
 let textError = document.querySelector(".errorEvent");
 let validationAnimation = false;
-
-// textError.style.display = "none";
+let div = $(".thanksMessage");
+div.hide();
 
 
 input.addEventListener("click", () => {
   $(input).removeClass("errorInputEmail");
   $(input).addClass("inputEmail");
-  if(validationAnimation == true){
+  if (validationAnimation == true) {
     validationAnimation = false;
     $(textError).animate({left: "935px"});
   }
 });
 
 boutonValidation.addEventListener("click", (event) => {
-  event.preventDefault(); 
+  event.preventDefault();
 
-  var inputValue = input.value; 
+  var inputValue = input.value;
   if (isValidEmail(inputValue)) {
-    console.log("Votre adresse e-mail est correcte");
+    emailThanks(inputValue);
   } else {
     $(input).addClass("errorInputEmail");
     $(textError).css("display", "flex");
@@ -29,9 +29,12 @@ boutonValidation.addEventListener("click", (event) => {
   }
 });
 
-
 function isValidEmail(email) {
   var emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   return emailRegex.test(email);
 }
 
+function emailThanks(email) {
+  // Code pour afficher le message de remerciement
+  $(div).show();
+}
