7298ab96baa5e8c4cc3d29fa92a22ce50661d8b4
Milan TURCANO <turcano.milan@sfr.fr>
2023-06-13 12:01:25 +0200

    Version mobile en cour. Bug condition de width. A continuer

--- main.js
+++ main.js
@@ -1,27 +1,30 @@
-// Variables 
+// Variables
 let input = document.querySelector(".inputEmail");
 let boutonValidation = document.querySelector(".button");
 let textError = document.querySelector(".errorEvent");
 let validationAnimation = false;
-let div = $(".thanksMessage");
+let div = document.querySelector(".thanksMessage");
 let container = document.querySelector(".container");
 let boutonSucess = document.querySelector(".buttonSucess");
-div.hide();
+div.style.display = "none";
+
 
 // Function for disable scroll
 function disableScroll() {
   document.body.style.overflow = "hidden";
 }
+
 // Event error email input
 input.addEventListener("click", () => {
-  $(input).removeClass("errorInputEmail");
-  $(input).addClass("inputEmail");
-  if (validationAnimation == true) {
+  input.classList.remove("errorInputEmail");
+  input.classList.add("inputEmail");
+  if (validationAnimation) {
     validationAnimation = false;
     $(textError).animate({ left: "935px" });
   }
 });
-// Event check email 
+
+// Event check email
 boutonValidation.addEventListener("click", (event) => {
   event.preventDefault();
 
@@ -29,31 +32,47 @@ boutonValidation.addEventListener("click", (event) => {
   if (isValidEmail(inputValue)) {
     emailThanks(inputValue);
   } else {
-    $(input).addClass("errorInputEmail");
-    $(textError).css("display", "flex");
+    input.classList.add("errorInputEmail");
+    textError.style.display = "flex";
     $(textError).animate({ left: "675px" });
     validationAnimation = true;
   }
 });
-// Event succes box and reload page
+
+// Event success box and reload page
 boutonSucess.addEventListener("click", () => {
   $(div).animate({ left: "-500px" }, 2000).css("display", "relative");
   $(container).animate({ top: "0px" }, 3500);
-  input.value = ""; 
+  input.value = "";
   setTimeout(() => {
     location.reload();
   }, 3800);
 });
-// function valid email
+
+// Function to validate email
 function isValidEmail(email) {
   var emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
   return emailRegex.test(email);
 }
-// Function email sub thanks
+
+// Function for email submission thanks
 function emailThanks(inputValue) {
-  $(div).show();
+  div.style.display = "block";
   $(container).animate({ bottom: "850px" }, 2000);
   $(div).animate({ top: "250px" }, 2000).css("display", "relative");
   $(".userEmail").text(inputValue);
   $(".userEmail").css("fontWeight", "bold");
 }
+
+function mobile(){
+  if(window.innerWidth <= 426){
+    console.log("Bonjour cela fonctionne")
+  } else {
+    console.log("aller nique ta mère");
+  }
+
+  let windowWidth = window.innerWidth;
+  console.log(windowWidth);
+}
+
+document.addEventListener("DOMContentLoaded", mobile);
